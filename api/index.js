878985ce1f0a6a6ff4e2a81671b683496886f85a
ERP App <erp@app.com>
2026-08-24 19:16:39 +0100

    Fix Vercel config: remove postinstall, fix rewrites, use outputDirectory

--- api/index.js
+++ api/index.js
@@ -3,9 +3,3 @@ import app from '../server/src/index.js';
 export default function handler(req, res) {
   return app(req, res);
 }
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
